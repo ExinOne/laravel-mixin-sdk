4f5ccc2d8597a586a3f362e8a280252615696262
Thorb <thorb@foxmail.com>
2019-12-18 14:44:30 +0800

    add private key config

--- config/publicConf.php
+++ config/publicConf.php
@@ -13,13 +13,13 @@ return [
     // 关键信息
     'keys'    => [
         'default' => [
-            'mixin_id'      => env('MIXIN_SDK_MIXIN_ID'),       //
-            'client_id'     => env('MIXIN_SDK_CLIENT_ID'),      //
-            'client_secret' => env('MIXIN_SDK_CLIENT_SECRET'),  //
-            'pin'           => env('MIXIN_SDK_PIN'),            //
-            'pin_token'     => env('MIXIN_SDK_PIN_TOKEN'),      //
-            'session_id'    => env('MIXIN_SDK_SESSION_ID'),     //
-            'private_key'   => '',                                    //import your private_key
+            'mixin_id'      => env('MIXIN_SDK_MIXIN_ID'),
+            'client_id'     => env('MIXIN_SDK_CLIENT_ID'),
+            'client_secret' => env('MIXIN_SDK_CLIENT_SECRET'),
+            'pin'           => env('MIXIN_SDK_PIN'),
+            'pin_token'     => env('MIXIN_SDK_PIN_TOKEN'),
+            'session_id'    => env('MIXIN_SDK_SESSION_ID'),
+            'private_key'   => str_replace("\\n", "\n", env('MIXIN_SDK_PRIVATE_KEY')),
         ],
     ],
 ];
